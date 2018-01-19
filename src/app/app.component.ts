9b788afdcc2b6f98c655e278dd938e33d807d7fd
SM Farhad Ali <forhad.univdhaka@gmail.com>
2018-01-19 14:06:05 +0100

    SHA 256 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+const sha256 = require('sha256');
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'BlockChain demo';
+  demo = sha256('BlockChain demo');
+  hash = sha256('h');
+  textValue = 'initial value';
+  log = '';
 }
